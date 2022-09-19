f4764df6769b33b3dd6e23d0cacfacf47c73aa19
hungleung <hungleung.lee@me.com>
2022-09-19 23:54:01 +0800

    更改自定义脚本和配置

--- jdc_lean.sh
+++ jdc_lean.sh
@@ -24,7 +24,7 @@ curl --retry 3 -s --globoff "https://gist.githubusercontent.com/1-1-2/335dbc8e13
 
 # fix2 + fix4.2
 # echo '修补 mt7621.mk'
-sed -i '/Device\/adslr_g7/i\define Device\/jdcloud_re-sp-01b\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 32448k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := RE-SP-01B\n  DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7603 kmod-mt7615e kmod-mt7615-firmware kmod-sdhci-mt7620 kmod-usb3 wpad-openssl\nendef\nTARGET_DEVICES += jdcloud_re-sp-01b\n\n' openwrt/target/linux/ramips/image/mt7621.mk
+sed -i '/Device\/adslr_g7/i\define Device\/jdcloud_re-sp-01b\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 32448k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := RE-SP-01B\n  DEVICE_PACKAGES := kmod-usb-storage kmod-usb2 block-mount kmod-fs-ext4 e2fsprogs fdisk kmod-sdhci-mt7620 kmod-usb3\nendef\nTARGET_DEVICES += jdcloud_re-sp-01b\n\n' openwrt/target/linux/ramips/image/mt7621.mk
 
 # fix3 + fix5.2
 # echo '修补 02-network'
@@ -35,11 +35,13 @@ sed -i -e '/lenovo,newifi-d1|\\/i\        jdcloud,re-sp-01b|\\' -e '/ramips_setu
 # echo '修补 system.sh 以正常读写 MAC'
 sed -i 's#key"'\''=//p'\''#& \| head -n1#' openwrt/package/base-files/files/lib/functions/system.sh
 
+sed -i -e 's/ddns-scripts_aliyun/luci-app-udpxy/' -e 's/ddns-scripts_dnspod/luci-app-wireguard/' -e 's/luci-app-ddns//' -e 's/luci-app-autoreboot/luci-app-qbittorrent/' -e 's/luci-app-arpbind/luci-app-nfs/' -e 's/luci-app-filetransfer/luci-app-samba4/' -e 's/luci-app-vsftpd/luci-app-minidlna/' -e 's/luci-app-accesscontrol//' -e 's/luci-app-nlbwmon//' -e 's/luci-app-wol//' openwrt/include/target.mk
+
 #=========================================
 # Target System
 #=========================================
-cat >> .config << EOF
-CONFIG_TARGET_ramips=y
-CONFIG_TARGET_ramips_mt7621=y
-CONFIG_TARGET_ramips_mt7621_DEVICE_jdcloud_re-sp-01b=y
-EOF
+# cat >> .config << EOF
+# CONFIG_TARGET_ramips=y
+# CONFIG_TARGET_ramips_mt7621=y
+# CONFIG_TARGET_ramips_mt7621_DEVICE_jdcloud_re-sp-01b=y
+# EOF
