18281cddd393463833b18c149ba11e1ce7b9f9e8
hungleung <hungleung.lee@me.com>
2022-10-15 22:01:22 +0800

    Update jdc_lean.sh

--- jdc_lean.sh
+++ jdc_lean.sh
@@ -1,49 +1,29 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm
-#=================================================
-#1. Modify default IP
-sed -i 's/192.168.1.1/192.168.88.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# 修改主机名为 JDC_Mark1
-sed -i 's/OpenWrt/Home/g' openwrt/package/base-files/files/bin/config_generate
+# For OpenWrt 21.02 or lower version
+# You have to manually upgrade Golang toolchain to 1.18 or higher to compile Xray-core.
+# ./scripts/feeds update packages
+# rm -rf feeds/packages/lang/golang
+# svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
+
+# change default lan address and hostname
+# verified to be working
+sed -i 's/192.168.1.1/192.168.88.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/Home/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/\+shellsync//' openwrt/package/network/services/ppp/Makefile
+sed -i 's/\+kmod-mppe//' openwrt/package/network/services/ppp/Makefile
+sed -i 's/"ShadowSocksR Plus+"/"SSRP+"/'  openwrt/feeds/helloworld/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
+
+# disable and remove wireless
 sed -i 's/\+libiwinfo-lua//' openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/iwinfo//' openwrt/feeds/luci/modules/luci-mod-admin-full/Makefile
 sed -i 's/wpad-openssl//' openwrt/target/linux/ramips/mt7621/target.mk
 
-#2. Clear the login password
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
-
-# load dts
-# echo '载入 mt7621_jdcloud_re-sp-01b.dts'
 curl --retry 3 -s --globoff "https://gist.githubusercontent.com/1-1-2/335dbc8e138f39fb8fe6243d424fe476/raw/[lean's%20lede]mt7621_jdcloud_re-sp-01b.dts" -o openwrt/target/linux/ramips/dts/mt7621_jdcloud_re-sp-01b.dts
-# ls -l target/linux/ramips/dts/mt7621_jdcloud_re-sp-01b.dts
-
-# fix2 + fix4.2
-# echo '修补 mt7621.mk'
-sed -i '/Device\/adslr_g7/i\define Device\/jdcloud_re-sp-01b\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 32448k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := RE-SP-01B\n  DEVICE_PACKAGES := lsblk block-mount kmod-fs-ext4 e2fsprogs fdisk kmod-sdhci-mt7620\nendef\nTARGET_DEVICES += jdcloud_re-sp-01b\n\n' openwrt/target/linux/ramips/image/mt7621.mk
-
-# fix3 + fix5.2
-# echo '修补 02-network'
+sed -i '/Device\/adslr_g7/i\define Device\/jdcloud_re-sp-01b\n  \$(Device\/dsa-migration)\n  \$(Device\/uimage-lzma-loader)\n  IMAGE_SIZE := 32448k\n  DEVICE_VENDOR := JDCloud\n  DEVICE_MODEL := RE-SP-01B\n  DEVICE_PACKAGES := lsblk block-mount e2fsprogs fdisk kmod-fs-ext4 kmod-sdhci-mt7620 kmod-usb3\nendef\nTARGET_DEVICES += jdcloud_re-sp-01b\n\n' openwrt/target/linux/ramips/image/mt7621.mk
 sed -i -e '/lenovo,newifi-d1|\\/i\        jdcloud,re-sp-01b|\\' -e '/ramips_setup_macs/,/}/{/ampedwireless,ally-00x19k/i\        jdcloud,re-sp-01b)\n\t\tlan_mac=$(mtd_get_mac_ascii u-boot-env mac)\n\t\twan_mac=$(macaddr_add "$lan_mac" 1)\n\t\tlabel_mac=$lan_mac\n\t\t;;
-}' openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/02_network
-
-# fix5.1
-# echo '修补 system.sh 以正常读写 MAC'
+    }' openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/02_network
 sed -i 's#key"'\''=//p'\''#& \| head -n1#' openwrt/package/base-files/files/lib/functions/system.sh
 
-sed -i -e 's/kmod-nf-nathelper//' -e 's/kmod-nf-nathelper-extra//' -e 's/iptables-mod-extra//' -e 's/kmod-ipt-raw//' -e 's/kmod-tun//' -e 's/ca-certificates//' -e 's/coremark//' -e 's/ddns-scripts_aliyun/luci-app-udpxy/' -e 's/ddns-scripts_dnspod/luci-app-wireguard/' -e 's/luci-app-ddns//' -e 's/luci-app-autoreboot//' -e 's/luci-app-arpbind//' -e 's/luci-app-filetransfer//' -e 's/luci-app-vsftpd//' -e 's/luci-app-accesscontrol//' -e 's/luci-app-nlbwmon//' -e 's/luci-app-wol//' openwrt/include/target.mk
-
-#=========================================
-# Target System
-#=========================================
-# cat >> .config << EOF
-# CONFIG_TARGET_ramips=y
-# CONFIG_TARGET_ramips_mt7621=y
-# CONFIG_TARGET_ramips_mt7621_DEVICE_jdcloud_re-sp-01b=y
-# EOF
+# change default package
+sed -i -e 's/ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns//' -e 's/luci-app-autoreboot//' -e 's/luci-app-arpbind luci-app-filetransfer luci-app-vsftpd/luci-app-udpxy/' -e 's/luci-app-accesscontrol luci-app-nlbwmon //' -e 's/luci-app-wol/luci-app-wireguard/'  openwrt/include/target.mk
